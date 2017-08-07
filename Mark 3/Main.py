cb52ff5d5f78b9399270af7d806821c09b1a1f36
JGEDonkatsu <brianpa2@hotmail.com>
2017-08-07 14:15:33 +0900

    Update Main.py

--- Mark 3/Main.py
+++ Mark 3/Main.py
@@ -4,14 +4,11 @@ Created on 2017. 7. 7.
 @author: DJ
 '''
 import sys
-import os
-import cv2, time
+import cv2
 import numpy as np
 
 from PyQt4 import QtGui, QtCore
 from UI import Ui_MainWindow
-from DownloadNAnalysis import DownNAnalyze
-from __builtin__ import int
 
 
 class Video():
@@ -39,50 +36,6 @@ class Video():
         except:
             return None
             
-
-class ProgressPopup(QtGui.QDialog):
-    Terminate = DownNAnalyze()
-    nProgress = QtCore.pyqtSignal(int)
-    def __init__(self, parent=None):
-        QtGui.QDialog.__init__(self,parent)
-        self.setWindowTitle("Analyizing In Process")
-        self.setGeometry(770,520,400,100)
-        self.progressBar = QtGui.QProgressBar(self)
-        self.progressBar.setGeometry(30,40,350,20)
-        self.button = QtGui.QPushButton("Press To Exit", self)
-        self.button.setGeometry(150,70,100,25)
-        self.button.clicked.connect(self.onStart)
-        self.qFlag = False
-
-    def onStart(self):
-        self.progressBar.setRange(0,100)
-        self.progressBar.setValue(0)
-        if(Ui_MainWindow.mArray == []):
-            print ("ZERO")
-        else:
-            aLength = len(Ui_MainWindow.mArray)
-            print aLength
-            tBlock = 50/aLength
-            print tBlock
-            for i in Ui_MainWindow.mArray:
-                self.Terminate.Start('dgutest01', i)
-                self.progressBar.setValue(tBlock)
-                tBlock = tBlock + tBlock
-            for j in Ui_MainWindow.mArray:
-                self.Terminate.Start('dgutest02', j)
-                self.progressBar.setValue(tBlock)
-                tBlock = tBlock + tBlock
-        
-        self.progressBar.setValue(100)
-        QtGui.qApp.processEvents()
-        exit()
-    
-    def closeEvent(self, event):
-        if self.qFlag:
-            super(QtGui.QDialog,self).closeEvent(event)
-        else:
-            event.ignore()
-            
 class Gui(QtGui.QMainWindow):
     video01 = Video(cv2.VideoCapture(0))
     video02 = Video(cv2.VideoCapture(1))
@@ -92,6 +45,7 @@ class Gui(QtGui.QMainWindow):
 
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+
         self._timer = QtCore.QTimer(self)
         self._timer.timeout.connect(self.play)
         self._timer.start(27)
@@ -113,9 +67,12 @@ class Gui(QtGui.QMainWindow):
             pass
 
     def closeEvent(self, event):
-        self.Popup()
+        Ui_MainWindow.LThread01.start()
+        Ui_MainWindow.LThread01.start()
+        
         Ui_MainWindow.vThread01.stop()
         Ui_MainWindow.vThread02.stop()
+
         self.Quit()
         
     def Quit(self):
@@ -123,11 +80,7 @@ class Gui(QtGui.QMainWindow):
         Ui_MainWindow.vThreadVideo01.release()
         Ui_MainWindow.vThreadVideo02.release()
         QtCore.QCoreApplication.quit()
-    
-    def Popup(self):
-        self.dialog = ProgressPopup()
-        self.dialog.exec_()
-    
+
 def main():
     app = QtGui.QApplication(sys.argv)
     ex = Gui()
