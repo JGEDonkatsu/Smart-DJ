caef86a17365820989642c2446980060d04ff6ba
sujineee <jsuejih@naver.com>
2017-08-07 16:16:17 +0900

    한글 주석 추가

--- Mark 3/CaptureNUpload.py
+++ Mark 3/CaptureNUpload.py
@@ -13,18 +13,17 @@ from common import clock, draw_str
 
 class CapNUp(QThread):
     tFlag = False
-    cCounter = 0
-    mCounter = 1
-    counter = 0 #upload
+    cCounter = 0 # while문 실행한 개수
+    counter = 0 # 실제 캡처한 이미지 개수
     fileNum = 0
-
     def __init__(self, video, fileName):
+        print("IN THREAD")
         QThread.__init__(self)        
         self.S3 = boto3.client('s3', config = Config(signature_version = 's3v4'))
         self.video = video
-        self.fileName = unicode(fileName)
+        self.fileName = unicode(fileName) # 한글 노래명
         self.slash = self.fileName.rfind('/')
-        self.mp3 = self.fileName.rfind('.wmv')
+        self.mp3 = self.fileName.rfind('.mp3')
         self.fileName = self.fileName[self.slash+1:self.mp3]
         self.now = time.localtime()
         self.today = str(self.now.tm_year)+"."+str(self.now.tm_mon)+"."+str(self.now.tm_mday)
@@ -32,11 +31,12 @@ class CapNUp(QThread):
     def __del__(self):
         self.wait()
     
-    def stop(self):
+    def stop(self): #스레드 종료
         print("END")
         self.tFlag = True
 
     def run(self):
+        # opencv 설정
         import sys, getopt
         args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade=', 'nested-cascade='])
         try:
@@ -57,10 +57,10 @@ class CapNUp(QThread):
         
         # Loop
         while(self.tFlag == False):
-            if(self.cCounter == 10):
+            if(self.cCounter == 10): # while문 10번 돌리면
                 self.cCounter = 0
-                if self.counter != 0:
-                    self.UploadToS3()
+                if self.counter != 0: # 얼굴 캡처 한번 이상 했으면 S3 업로드
+                    self.UploadToS3() 
                 print "cCounter END"
                 break
             
@@ -78,12 +78,12 @@ class CapNUp(QThread):
                         vis_roi = vis[y1:y2, x1:x2]
                         self.fileNum += 1
                         self.counter += 1
-                        zoom = cv2.resize(vis_roi, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC) #Image Zoom
-                        cv2.imwrite('uploader/{0}.jpg'.format(self.fileNum), zoom)
+                        zoom = cv2.resize(vis_roi, None, fx=3, fy=3, interpolation=cv2.INTER_CUBIC) # 이미지 세배 확대
+                        cv2.imwrite('uploader/{0}.jpg'.format(self.fileNum), zoom) # 로컬PC에 저장
                         print "OK"
                         
-                dt = clock() - t
-                draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+                dt = clock() - t 
+                draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000)) # 한 화면에서 한 번에 여러 사람들 캡처 가능
                 
             except:
                 pass
@@ -98,6 +98,7 @@ class CapNUp(QThread):
         rects[:,2:] += rects[:,:2]
         return rects
     
+    # S3에 이미지 업로드
     def UploadToS3(self):
         Num = self.fileNum - self.counter
         print("UPLOADING...")
@@ -107,14 +108,11 @@ class CapNUp(QThread):
             
             print "Cam01 file counter :", i+1
             data.close()
-        wData = open('uploader/Weather.xls','rb')
-        self.S3.put_object(ACL = 'public-read', Bucket = 'dgutest01', Key = self.today+'/'+self.fileName+'/'+'Weather.xls', Body = wData)
-        wData.close()
         self.counter = 0
         print "Cam01 uploading end"
         self.Deletion(Num, self.fileNum)
-
     
+    # 로컬PC에서 업로드한 이미지 삭제
     def Deletion(self, start, end):
         for i in range(start, end):
             path = str('uploader/{0}.jpg'.format(i+1))
